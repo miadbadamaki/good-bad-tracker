d9afcdd3c0bcd2d105dcfdceb2d04fc93855e57f
miaad badamaki <mbadamaki31@gmail.com>
2026-02-15 23:47:37 +0330

    feat: inital project setup

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            }
+
+        }
+
+
+    }
+
